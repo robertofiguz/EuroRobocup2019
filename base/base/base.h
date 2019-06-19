18567f7dbc999a638b55e0ddc9445546ab6aadb4
Roberto Figueiredo <roberto2000@icloud.com>
2019-06-19 20:33:57 +0100

    version 2.0 library: update of library file header for sonar

--- base/base/base.h
+++ base/base/base.h
@@ -2,26 +2,39 @@
   base.h - Library for Controlling the base.
   Created by Roberto Figueiredo, June 18, 2019.
 */
+
 #ifndef base_h
 #define base_h
 
 #include "Arduino.h"
-#include "Servo.h"
+//#include "Servo.h" // include if end up creating class for the arms
 
 class base
 {
 public:
+
+  //bussola
     #define ADDRESS 0x60
     byte highByte, lowByte, fine;
     int bearing, pitch, roll;
     void ler_bussola();
+//-------------------
 
+//funções movimento
     void andarfrente(int tempo);
     void andartras(int tempo);
+    void andaresquerda(int tempo);
+    void andardireita(int tempo);
     void rodarCW(int tempo);
     void rodarCCW(int tempo);
     void parar(int tempo);
     void rodarPublico();
+//-------------------
+    bool medir();
+    int trigPin;
+    int echoPin;
+    long duration;
+    int distance;
     int motor1_1;
     int motor1_2;
     int motor2_1;
@@ -30,9 +43,18 @@ public:
     int motor3_2;
     int motor4_1;
     int motor4_2;
+    int vel1;
+    int vel2;
+    int vel3;
+    int vel4;
+    int vel5;
+    int vel6;
+    int vel7;
+    int vel8;
     base(int motor1_1, int motor1_2, int motor2_1, int motor2_2, int motor3_1, int motor3_2, int motor4_1, int motor4_2);
-  private:
     void writeToMotors(int vel1, int vel2, int vel3, int vel4, int vel5, int vel6, int vel7, int vel8);//check if this can be on private
+  private:
+
 
 
 };/*
