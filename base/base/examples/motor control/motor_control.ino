b3547f4f560fcdbd5260523f738deed8a83dad96
Roberto Figueiredo <robertofigueiredo2000@gmail.com>
2022-06-15 18:04:10 +0100

    examples

--- base/base/examples/motor control/motor_control.ino
+++ base/base/examples/motor control/motor_control.ino
@@ -1,25 +1,18 @@
 #include<base.h> // includes base library
 #include<Wire.h>
 
-base Base(); // sets up the pins in the order: motor1, motor1, motor2, motor2, motor3, moto3, motor4, motor4
+base Base(6,7,8,9,10,11,4,5);  // sets up the pins in the order: motor1, motor1, motor2, motor2, motor3, moto3, motor4, motor4
 
 void setup() {
   Serial.begin(9600);
-
-
 }
 
 void loop() {
-  Base.ler_bussola();
-  //returns degrees
-
-  //velocidade range: 0-100
   Base.andarfrente(tempo, velocidade);
   Base.andartras(tempo, velocidade);
   Base.andaresquerda(tempo, velocidade);
   Base.andardireita(tempo, velocidade);
-  Base.rodar(angulo) //angulo entre -180 e 180
+  Base.rodar(angulo) 
   Base.parar(tempo);
-  Base.send(data); //data needs to be integer
 
 }
